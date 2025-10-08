02efeb67db5493ffd8097113c9ab4fa2a85ff29c
swarnapriya <swaranasaravanan02@gmail.com>
2025-10-08 11:27:38 +0530

    Success

---
+++ swarna/myapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+def calculate_BMI(request):
+  context = {}
+  context['BMI'] = "0"
+  context['height'] = "0"
+  context['weight'] = "0"
+  if (request.method=="POST"):
+    print("POST method is used")
+    height = request.POST.get("height","0")
+    weight = request.POST.get("weight","0")
+    print("Request=", request)
+    print("Height=", height)
+    print("Weight=", weight)
+    BMI = float(weight)/(float(height)*float(height))
+    context['BMI'] = BMI
+    context['height'] = height
+    context['weight'] = weight
+    print("BMI=", BMI)
+
+  
+  return render (request,'myapp/math.html',context)
---
+++ swarna/swarna/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('calculate_BMI/', views.calculate_BMI, name = "BMI"),
+    path('', views.calculate_BMI, name="BMIroot")
+]
